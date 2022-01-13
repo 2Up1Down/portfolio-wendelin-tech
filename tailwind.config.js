08d2e0d8939bd2ec8a3a9563147d09d26a6fd7bb
Wendelin <wendelin.steiner@gmail.com>
2022-01-13 15:28:57 +0100

    Added prettier, tailwindcss and moved everything to src

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/pages/**/*.{html,js}", "./src/components/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      Poppins: ["Poppins, sans-serif"],
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
