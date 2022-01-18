88132ece9d92e29fe8e1c52bae24c30fab7e82ac
Wendelin <wendelin.steiner@gmail.com>
2022-01-18 21:38:36 +0100

    added locale to locations query

--- src/utils/contentful/query-locations.js
+++ src/utils/contentful/query-locations.js
@@ -4,11 +4,12 @@ import { client } from "../apollo-client";
 // Code inspired by vercels cms-contentful example
 // https://github.com/vercel/next.js/blob/canary/examples/cms-contentful/lib/api.js
 
-const GET_LOCATIONS_OVERVIEW = gql`
+const GET_LOCATIONS_OVERVIEW = (locale) => gql`
   query getLocations {
-    contentTypeLocationCollection {
+    contentTypeLocationCollection(locale: "${locale}") {
       items {
         title
+        description
       }
     }
   }
@@ -18,10 +19,10 @@ function extractLocationEntries(fetchResponse) {
   return fetchResponse?.data?.contentTypeLocationCollection?.items;
 }
 
-export async function getLocationsOverview() {
+export async function getLocationsOverview(locale) {
   try {
     const entries = await client.query({
-      query: GET_LOCATIONS_OVERVIEW,
+      query: GET_LOCATIONS_OVERVIEW(locale),
     });
 
     return extractLocationEntries(entries);
