a19c9a6690f99c8f253949533f3c0159a8ede1c0
Wendelin <wendelin.steiner@gmail.com>
2022-02-03 10:44:59 +0100

    fixed bug where horizontal scrolling interfered with page design

--- src/components/common/json-stringify.js
+++ src/components/common/json-stringify.js
@@ -3,7 +3,7 @@ import React from "react";
 const JsonStringify = ({ jsonObj }) => {
   return (
     <section className="bg-lime-100 ">
-      <div className="container py-16">
+      <div className="container py-16 overflow-x-auto">
         <pre>{JSON.stringify(jsonObj, null, 2)}</pre>
       </div>
     </section>
