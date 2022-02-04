f750185f4c0d15d4689f02fe72f2d90c17aa3e50
Wendelin <wendelin.steiner@gmail.com>
2022-02-04 10:34:27 +0100

    added first draft for SocialMedia component

---
+++ src/components/common/social-media.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SocialMedia = () => {
+  return (
+    <div className="text-accent">Facebook | Twitter | LinkedIn | Github</div>
+  );
+};
+
+export default SocialMedia;
