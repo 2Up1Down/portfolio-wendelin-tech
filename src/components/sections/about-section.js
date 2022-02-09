447142586b61726ae9261dcbf5f90dac70a73478
Wendelin <wendelin.steiner@gmail.com>
2022-02-09 16:20:58 +0100

    refactored about section

--- src/components/sections/about-section.js
+++ src/components/sections/about-section.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import { renderTitleOptions } from "../../styles/contentful-rich-text";
-import Image from "next/image";
 import SocialMedia from "../common/social-media";
 
 const AboutMeSection = ({ title, description, image }) => {
   return (
     <section className="bg-gray-light">
-      <div className="pt-12 md:py-0 md:grid grid-cols-full-width grid-area">
-        <div className="grid-text px-4 pb-8 self-center md:pb-0">
+      <div className="pt-12 md:py-0 md:grid grid-cols-full-width area-about">
+        <div className="grid-about px-4 pb-8 self-center md:pb-0">
           <div className="mb-8">
             {title &&
               documentToReactComponents(title.json, renderTitleOptions())}
@@ -19,16 +18,11 @@ const AboutMeSection = ({ title, description, image }) => {
           </div>
           <SocialMedia />
         </div>
-        <div className="grid-media">
-          <div className="relative w-full h-80 md:min-h-[700px]">
-            <Image
-              src={image.url}
-              alt={image.title}
-              layout="fill"
-              objectFit="cover"
-            />
-          </div>
-        </div>
+        {/* inspired by https://stackoverflow.com/questions/70805041/background-image-in-tailwindcss-using-dynamic-url-react-js */}
+        <div
+          className="grid-media bg-primary-light bg-no-repeat bg-cover bg-center h-80 md:min-h-[700px]"
+          style={{ backgroundImage: `url(${image.url})` }}
+        />
       </div>
     </section>
   );
