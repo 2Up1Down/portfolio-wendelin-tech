20764852f2b5c51c51231b47dc8b2aa0ed3fa347
Wendelin <wendelin.steiner@gmail.com>
2022-02-15 11:08:35 +0100

    added floating label to form textarea

--- src/components/forms/form-textarea.js
+++ src/components/forms/form-textarea.js
@@ -7,17 +7,24 @@ const FormTextarea = ({
   placeholder = "",
   required = false,
 }) => {
+  // inspired by "Floating Labels with Tailwind CSS" https://youtu.be/nJzKi6oIvBA
   return (
-    <>
-      <label htmlFor={name}>{label}</label>
+    <div className="relative">
       <textarea
         rows={rows}
         name={name}
         placeholder={placeholder}
         required={required}
-        className="mb-4"
+        // className="mb-4 relative"
+        className="peer pt-3 mb-4 w-full px-2 placeholder-transparent focus:outline-none"
       />
-    </>
+      <label
+        htmlFor={name}
+        className="opacity-70 absolute left-2 -top-0.5 text-xs font-medium text-text-base peer-placeholder-shown:text-base peer-placeholder-shown:opacity-50 peer-placeholder-shown:top-3 peer-focus:-top-0.5 peer-focus:text-xs peer-focus:opacity-70  transition-all select-none pointer-events-none"
+      >
+        {label}
+      </label>
+    </div>
   );
 };
 
