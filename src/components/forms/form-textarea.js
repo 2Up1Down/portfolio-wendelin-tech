0d96c4b4134b2366fad9273b94ace946adac8ea2
Wendelin <wendelin.steiner@gmail.com>
2022-02-16 09:27:48 +0100

    test - sorted tailwind classes with prettier plugin

--- src/components/forms/form-textarea.js
+++ src/components/forms/form-textarea.js
@@ -16,11 +16,11 @@ const FormTextarea = ({
         placeholder={placeholder}
         required={required}
         // className="mb-4 relative"
-        className="peer pt-3 mb-4 w-full px-2 placeholder-transparent focus:outline-none"
+        className="peer mb-4 w-full px-2 pt-3 placeholder-transparent focus:outline-none"
       />
       <label
         htmlFor={name}
-        className="opacity-70 absolute left-2 -top-0.5 text-xs font-medium text-text-base peer-placeholder-shown:text-base peer-placeholder-shown:opacity-50 peer-placeholder-shown:top-3 peer-focus:-top-0.5 peer-focus:text-xs peer-focus:opacity-70  transition-all select-none pointer-events-none"
+        className="pointer-events-none absolute left-2 -top-0.5 select-none text-xs font-medium text-text-base opacity-70 transition-all peer-placeholder-shown:top-3 peer-placeholder-shown:text-base peer-placeholder-shown:opacity-50  peer-focus:-top-0.5 peer-focus:text-xs peer-focus:opacity-70"
       >
         {label}
       </label>
