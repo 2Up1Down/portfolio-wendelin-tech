8b55c6f728e9f7d5015ec888278191a9584a7e97
Wendelin <wendelin.steiner@gmail.com>
2022-02-17 14:06:32 +0100

    added language switch for desktop

--- src/components/common/locale-switcher.js
+++ src/components/common/locale-switcher.js
@@ -1,27 +1,44 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { MdKeyboardArrowDown } from "react-icons/md";
 
 const LocaleSwitcher = () => {
   const router = useRouter();
-  const { locales, locale: activeLocale } = router;
-  const otherLocales = locales.filter((locale) => locale !== activeLocale);
+  const { locales, locale: activeLocale, pathname, query, asPath } = router;
 
   return (
-    <>
-      <ul className="flex flex-row">
-        {otherLocales.map((locale) => {
-          const { pathname, query, asPath } = router;
-
+    <div className="group relative">
+      <Link href={{ pathname, query }} as={asPath} locale={activeLocale}>
+        <a>
+          <div className="flex place-items-center uppercase ">
+            <span>{activeLocale}</span>
+            <span>
+              <MdKeyboardArrowDown />
+            </span>
+          </div>
+        </a>
+      </Link>
+      <ul className="absolute right-0 hidden origin-top-right -translate-x-2 flex-col justify-start bg-white py-2 shadow group-hover:flex">
+        {locales.map((locale) => {
           return (
-            <li className="mr-4 last:mr-0" key={locale}>
+            <li
+              className={`my-1 px-2 ${
+                locale === activeLocale && "bg-gray-light"
+              }  hover:bg-gray-light`}
+              key={locale}
+            >
               <Link href={{ pathname, query }} as={asPath} locale={locale}>
-                <a>{locale.toUpperCase()}</a>
+                <a>
+                  <div className="flex place-items-center uppercase ">
+                    <span>{locale}</span>
+                  </div>
+                </a>
               </Link>
             </li>
           );
         })}
       </ul>
-    </>
+    </div>
   );
 };
 
