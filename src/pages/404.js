294851305df59404a4c33896eaec84a2c628b43d
Wendelin <wendelin.steiner@gmail.com>
2022-02-21 11:20:42 +0100

    added layout to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,31 @@
 import React from "react";
 import Layout from "../components/common/layout";
+import { getLayout } from "../utils/contentful/query-layout";
 
-const NotFoundPage = () => {
+const NotFoundPage = ({ header, footer }) => {
   return (
-    <Layout>
-      <div className="container">--- Page not found ---</div>
+    <Layout header={header} footer={footer}>
+      <div className="">
+        <div className="container grid h-screen place-items-center">
+          <h1 className="text-2xl">
+            <span className="font-bold">404</span> - Page Not Found
+          </h1>
+        </div>
+      </div>
     </Layout>
   );
 };
 
 export default NotFoundPage;
+
+export async function getStaticProps({ locale }) {
+  const { header, footer } = await getLayout(locale);
+
+  return {
+    props: {
+      header,
+      footer,
+    },
+    revalidate: 60, // in seconds
+  };
+}
