3834d02fbd2776a8bb0be3190755ee771345aa9f
Wendelin <wendelin.steiner@gmail.com>
2022-03-07 19:47:02 +0100

    Added illustrations to what-i-do-section

--- src/components/sections/what-i-do-section.js
+++ src/components/sections/what-i-do-section.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import { renderTitleOptions } from "../../styles/contentful-rich-text";
+import Image from "next/image";
 
 const WhatIDoSection = ({ title, description }) => {
   return (
     <section className="bg-primary">
-      <div className="container h-screen grid place-items-center">
-        <div className="max-w-lg text-gray-light text-left">
+      <div className="container relative grid h-screen place-items-center">
+        <div className=" max-w-lg text-left text-gray-light">
           <div className="text-text-light mb-6">
             {title &&
               documentToReactComponents(title.json, renderTitleOptions())}
@@ -15,6 +16,33 @@ const WhatIDoSection = ({ title, description }) => {
             {description &&
               documentToReactComponents(description.json, renderTitleOptions())}
           </div>
+        </div>
+
+        <div className="absolute translate-x-[35rem] translate-y-[10rem]">
+          <Image
+            src="/dart.svg"
+            alt="Illustration from dart board"
+            width={154}
+            height={228}
+          />
+        </div>
+
+        <div className="absolute -translate-x-[30rem] translate-y-[18rem]">
+          <Image
+            src="/graph.svg"
+            alt="Illustration from dart board"
+            width={104}
+            height={103}
+          />
+        </div>
+
+        <div className="absolute -translate-x-[40rem] -translate-y-[10rem]">
+          <Image
+            src="/idea.svg"
+            alt="Illustration from dart board"
+            width={150}
+            height={153}
+          />
         </div>
       </div>
     </section>
